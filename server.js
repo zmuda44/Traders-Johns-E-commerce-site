d13cc9811582078fcc74985179d4507d89679ed6
Greg Bailey <gregbailey617@gmail.com>
2024-06-05 20:57:08 -0400

    Updated server to handlebars

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
