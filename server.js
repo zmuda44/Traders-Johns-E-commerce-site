11e8495371fc3015d41944694a0ac7754de891d0
Greg Bailey <gregbailey617@gmail.com>
2024-06-06 18:58:41 -0400

    First commit to render

--- server.js
+++ server.js
@@ -1,17 +1,45 @@
-const express = require('express');
-const app = express();
-const exphbs = require('express-handlebars');
 const path = require('path');
+const express = require("express");
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 
-const PORT = process.env.PORT || 3001;
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = 3001;
+
+const hbs = exphbs.create({});
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-  );
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () =>
-    console.log(`App listening at http://localhost:${PORT}`)
-  );
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
+
