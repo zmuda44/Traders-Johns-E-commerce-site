54a058de64ad279153cafa5b91edf04362e2253b
Greg Bailey <gregbailey617@gmail.com>
2024-06-06 20:59:31 -0400

    Accepted incoming change on models/index.js

--- models/index.js
+++ models/index.js
@@ -2,10 +2,6 @@ const Product = require('./Product');
 const Category = require('./Category');
 
 Category.hasMany(Product, {
-<<<<<<< HEAD
-  foreignKey: 'category_id',
-=======
->>>>>>> 7a5e3d02d94d16dbdb02f87912d463814f017cff
   onDelete: 'CASCADE'
 });
 
