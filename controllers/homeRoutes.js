3a7581602dc4301686348149d5497da3b383ac42
Greg <gregbailey617@gmal.com>
2024-06-09 16:07:54 -0400

    Created profile page and forms. Post request not working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -20,4 +20,17 @@ router.get('/login', (req, res) => {
     res.render('login');
 })
 
+router.get('/profile', async (req, res) => {
+    try {
+        const productData = await Product.findAll();
+
+        const products = productData.map((product) => product.get({ plain: true }));
+
+        res.render('profile', { products });
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }    
+})
+
 module.exports = router;
