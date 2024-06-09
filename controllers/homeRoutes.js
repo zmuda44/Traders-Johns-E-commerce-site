947da342232a2321bd96daabd023c1b6b6520639
Greg <gregbailey617@gmal.com>
2024-06-09 11:31:39 -0400

    Got home and login page working

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,23 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { Product, User } = require('../models');
 // const withAuth = require('../utils/auth');
 
 
-router.use('/', (req, res) => {
-    res.render('homepage', putPage[0]);
+router.get('/', async (req, res) => {
+    try {
+        const productData = await Product.findAll();
+
+        const products = productData.map((product) => product.get({ plain: true }));
+
+        res.render('homepage', {products});
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
 })
+
+router.get('/login', (req, res) => {
+    res.render('login');
+})
+
+module.exports = router;
