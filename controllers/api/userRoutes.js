1169355823c717e0b2977e08d3f5b2e3ff5f4d27
Greg <gregbailey617@gmal.com>
2024-06-10 19:31:24 -0400

    Cleaned up userRoutes.js after merge

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -24,22 +24,11 @@ router.post('/', async (req, res) => {
 
 
 
-module.exports = router;
 
-=======
 
-// try {
-  //   const userData = await User.create(req.body);
-  
-  //   req.session.save(() => {
-    //     req.session.user_id = userData.id;
-    //     req.session.logged_in = true;      
-    //   });
-    
-    //   res.status(200).json(userData);
-    // } catch (err) {
-      //   res.status(400).json(err);
-      // }
+
+
+
       
 router.post('/logout', (req, res) => {
   if (req.session.logged_in) {
