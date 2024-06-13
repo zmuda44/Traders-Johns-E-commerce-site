48eaf8657829b660b3f81d49273691e176bbfae2
Greg <gregbailey617@gmal.com>
2024-06-12 21:31:45 -0400

    tried to add post route but can't

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,23 +1,39 @@
 const router = require('express').Router();
-const { Product, User } = require('../models');
-// const withAuth = require('../utils/auth');
+const { Product, User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 
 
 // router.post('/', withAuth, async (req, res) => {
 
-router.post('/', async (req, res) => {
-    try {
-      const newProject = await Product.create({
-        ...req.body
-        // user_id: req.session.user_id,
-      });
+router.post('/', (req, res) => {
+  console.log("hello")
+});
+  
+  // try {
+  //     const newProject = await Product.create(req.body);
       
   
-      res.status(200).json("hello");
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  });
+  //     res.status(200).json("hello");
+  //   } catch (err) {
+  //     res.status(400).json(err);
+  //   }
+
 
 module.exports = router;
+
+// router.post('/', withAuth, async (req, res) => {
+//   console.log("hello")
+  
+//   try {
+//       const newProject = await Product.create({
+//         ...req.body,
+//         user_id: req.session.user_id,
+//       });
+      
+  
+//       res.status(200).json("hello");
+//     } catch (err) {
+//       res.status(400).json(err);
+//     }
+//   });
