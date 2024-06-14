091eca6c603feb338a3cbc0c59a0fcd856c775a9
Greg <gregbailey617@gmal.com>
2024-06-14 11:12:29 -0400

    Added new route and handlebars to show price in euros

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Product, User, Category } = require('../models');
 const withAuth = require('../utils/auth');
-
+const { Convert } = require("easy-currencies");
 
 
 router.get('/', async (req, res) => {
@@ -62,5 +62,43 @@ router.get('/products/:category_id', async (req,res) =>{
       res.status(500).json(err);
     }
   })
+
+  const convertCurrency = async (price) => {
+    try {
+        // Assuming you have a function Convert() that performs currency conversion
+        const convertedPrice = await Convert(price).from("USD").to("EUR");
+        return convertedPrice;
+    } catch (error) {
+        console.error("Error converting currency:", error);
+        throw error;
+    }
+};
+
+  router.get('/euros', async (req, res) => {
+
+    try {
+        const productData = await Product.findAll({
+            include: [{
+                model: Category,
+                attributes: ['category_name'],
+            }] // Include the Category model to fetch associated categories
+        });
+
+        const productPromises = productData.map(async (product) => {
+            const convertedPrice = await convertCurrency(product.price);
+            return {
+                ...product.get({ plain: true }),
+                convertedPrice
+            };
+        });
+
+        const products = await Promise.all(productPromises);
+       
+
+        res.render('euros-homepage', { products, logged_in: req.session.logged_in });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
