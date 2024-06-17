4a84dd6b5934973172b9f49aa648a91a9f863641
Greg <gregbailey617@gmal.com>
2024-06-16 21:55:13 -0400

    Added functionality to site but couldn't test locally because of CORS

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -6,16 +6,37 @@ const submitBtn = document.getElementById('credit-card-btn')
 
 //Click on checkout to send credit card and item information to credit card merchant site
 
-function sendCcreditCardInfo () {
-    e.preventDefault()
-    const name = ccNameEl.value;
-    const number = ccNumberEl.value;
-    const billing = ccBillingEl.value;
-    const product = productEl.textContent
+const sendcreditCardInfo = async (e) => {
+    try {
+        e.preventDefault()
+        const name = ccNameEl.value;
+        const number = ccNumberEl.value;
+        const billing = ccBillingEl.value;
+        const product = productEl.textContent
 
-    console.log(name, number, billing, product)
+        if (name && number && billing) {
+            const response = await fetch('http://localhost:3000/', {
+                method: 'POST',
+                body: JSON.stringify({ name, number, billing, product }),
+                headers: { 'Content-Type': 'application/json' },
+            })
+            console.log(response);
+            if (response.ok) {
+                document.location.replace('/profile');
+            } else {
+                const errorData = await response.json();
+                alert(`Error: ${errorData.message || response.statusText}`);
+            }
+        } else {
+            alert('Transaction failed please check information');
+        }
+    } catch (error) {
+        console.error(error);
+    };
+};
 
-}
+
+submitBtn.addEventListener('click', sendcreditCardInfo)
 
 
 
