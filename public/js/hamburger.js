886b13215140a28019007f66904ac26fc52b1c02
Greg <gregbailey617@gmal.com>
2024-06-26 23:52:27 -0400

    not sure what this was.  last commit on dell amd laptop

--- public/js/hamburger.js
+++ public/js/hamburger.js
@@ -1,6 +1,5 @@
 document.querySelector(".hamburger-menu").addEventListener("click", function () {
     document.querySelector(".header-btn-container").classList.toggle("active");
     this.classList.toggle("active");
-
 })
 
